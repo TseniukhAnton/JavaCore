d69435c041d61be34fbc47b5208d5655d64b8d83
Anton <anton.tseniukh@gmail.com>
2021-09-15 09:23:45 +0300

    add Stream API reduce

--- src/main/java/com/TseniukhAnton/javacore/chapter29/StreamDemo.java
+++ src/main/java/com/TseniukhAnton/javacore/chapter29/StreamDemo.java
@@ -42,3 +42,36 @@ public class StreamDemo {
         System.out.println();
     }
 }
+
+
+class StreamDemo2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> myList = new ArrayList<>();
+        myList.add(7);
+        myList.add(18);
+        myList.add(10);
+        myList.add(24);
+        myList.add(17);
+        myList.add(5);
+
+        Optional<Integer> productObj = myList.stream().reduce((a, b) -> a * b);
+        if (productObj.isPresent())
+            System.out.println("Multiply object type Optional: " + productObj.get());
+
+        int product = myList.stream().reduce(1, (a, b) -> a * b);
+        System.out.println("Multiply int values: " + product);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
